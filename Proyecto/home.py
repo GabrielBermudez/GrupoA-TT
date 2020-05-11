d81a9470b3485dec3d8d078f244179b1466f16a2
Ramiro Gonzalez Boriero <ramiro.gonzalez@alumnos.frm.utn.edu.ar>
2020-05-11 02:06:29 -0300

    Modifico front end del home

--- Proyecto/home.py
+++ Proyecto/home.py
@@ -16,35 +16,36 @@ class Luxury:
         self.ventanaHome.title("LUXURY")
         self.ventanaHome.geometry("800x600")
         self.ventanaHome.resizable(0,0)
+        self.ventanaHome.configure(bg= 'black')
         self.frameTitulo=ttk.Label(self.ventanaHome, text="Luxury")
-        self.frameTitulo.config(foreground="black",font=("Verdana",24))
+        self.frameTitulo.config(background = "black", foreground="white",font=("Verdana",32))
         self.frameTitulo.pack(anchor=CENTER)
 
         #creacion de los botones(sin asignar una funcion aun)
-        self.botonIngresar=tk.Button(self.ventanaHome, text="Ingresar", command= self.AbrirLogin)
-        self.botonIngresar.place(x=600, y=130, width=120, height=70)
+        self.botonIngresar=tk.Button(self.ventanaHome, text="Ingresar", command= self.AbrirLogin, bg="black", fg="white", font=("Verdana",16), relief= RAISED)
+        self.botonIngresar.place(x=550, y=150, width=210, height=105)
         
-        self.botonRegistrarse=tk.Button(self.ventanaHome, text="Registrarse", command= self.AbrirRegistro)
-        self.botonRegistrarse.place(x=600, y=230, width=120, height=70)
+        self.botonRegistrarse=tk.Button(self.ventanaHome, text="Registrarse", command= self.AbrirRegistro, bg="black", fg="white", font=("Verdana",16), relief= RAISED)
+        self.botonRegistrarse.place(x=550, y=300, width=210, height=105)
 
         #creacion de label
         #fecha
         
         self.fecha = date.today()
-        self.fecha1=tk.Label(self.ventanaHome, text=self.fecha)
-        self.fecha1.place(x=300, y=410, width=120, height=50)
-        self.fecha1.configure(foreground="red")
+        self.fecha1=tk.Label(self.ventanaHome, text=self.fecha, font=("Verdana",16))
+        self.fecha1.place(x=250, y=470, width=160, height=50)
+        self.fecha1.configure(foreground="white", background="black",relief=RAISED, bd = 10)
         #hora
-        self.hora1=tk.Label(self.ventanaHome, text="")
-        self.hora1.place(x=300, y=460, width=120, height=50)
-        self.hora1.configure(foreground="red")
+        self.hora1=tk.Label(self.ventanaHome, text="", font=("Verdana",16))
+        self.hora1.place(x=250, y=530, width=160, height=50)
+        self.hora1.configure(foreground="white", background="black", relief=RAISED, bd = 10)
         self.tiempo() 
 
         self.ventanaHome.mainloop()
     
     def tiempo(self):
         self.hora = tm.strftime('%H:%M:%S')
-        self.hora1.config(text=self.hora, fg="red")
+        self.hora1.config(text=self.hora, fg="white", bg="black")
         self.hora1.after(200,self.tiempo)
 
     def AbrirLogin(self):
